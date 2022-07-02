9d2653d6b4f17d233e8fb25ac3971ee159d63cec
Fabio de Melo <fabiomessias98@outlook.com>
2022-07-02 03:05:09 -0300

    Finalizado estrutura da pagina Fale Conosco

--- wp-content/themes/wp-bootstrap-starter-child/page-fale-conosco.php
+++ wp-content/themes/wp-bootstrap-starter-child/page-fale-conosco.php
@@ -45,7 +45,7 @@ get_header(); ?>
 
         <div class="row">
 
-            <div class="col-12">
+            <div class="col-12 mb-5">
 
                 <ul class="d-flex justify-content-center pl-0">
 
@@ -75,17 +75,61 @@ get_header(); ?>
                 </ul>
             </div>
 
-            <div class="col-12">
+            <div class="col-12 mb-5">
 
                 <div class="row">
 
-                    <div class="col-6">
+                    <div class="col-md-6 mb-4 mb-md-0">
 
-                        <h4>
+                        <h4 class="u-font-weight-bold u-color-folk-squid-ink mb-4">
                             Fale conosco
                         </h4>
+
+                        <p class="u-font-size-14 u-font-weight-regular u-color-folk-aluminium">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            Placeat consectetur eos est nemo eligendi, commodi quibusdam 
+                            iusto odio quia in corrupti culpa inventore, nam sequi earum, 
+                            non nesciunt obcaecati saepe!
+                        </p>
+                    </div>
+
+                    <div class="col-md-6">
+
+                        <form>
+
+                            <div class="row justify-content-end">
+
+                                <div class="col-md-6 my-2">
+                                    <input class="w-100 border-0 rounded d-block u-font-size-12 u-font-weight-regular u-bg-folk-light-gray p-3" type="text" placeholder="Primeiro nome:">
+                                </div>
+
+                                <div class="col-md-6 my-2">
+                                    <input class="w-100 border-0 rounded d-block u-font-size-12 u-font-weight-regular u-bg-folk-light-gray p-3" type="text" placeholder="último nome:">
+                                </div>
+
+                                <div class="col-md-6 my-2">
+                                    <input class="w-100 border-0 rounded d-block u-font-size-12 u-font-weight-regular u-bg-folk-light-gray p-3" type="email" placeholder="E-mail:">
+                                </div>
+
+                                <div class="col-md-6 my-2">
+                                    <input class="w-100 border-0 rounded d-block u-font-size-12 u-font-weight-regular u-bg-folk-light-gray p-3" type="text" placeholder="Telefone:">
+                                </div>
+
+                                <div class="col-12 my-2">
+                                    <textarea class="w-100 border-0 rounded d-block u-font-size-12 u-font-weight-regular u-bg-folk-light-gray p-3" style="height:180px" placeholder="Telefone:"></textarea>
+                                </div>
+
+                                <div class="col-6 col-md-4 my-2">
+                                    <input class="w-100 border-0 rounded d-block u-font-size-15 u-font-weight-bold text-center u-color-folk-white u-bg-folk-golden hover:u-bg-folk-squid-ink py-3" type="submit" value="Enviar"> 
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
+            </div>
+
+            <div class="col-12">
+                <iframe class="w-100 "src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d638.702406333728!2d-50.12486562302182!3d-25.10028988982298!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94e81ba08536021f%3A0x6fcb650a99d496fd!2sR.%20Doral%C3%ADcio%20Correia%2C%20357%20-%20Uvaranas%2C%20Ponta%20Grossa%20-%20PR%2C%2084031-190!5e0!3m2!1spt-BR!2sbr!4v1655956105383!5m2!1spt-BR!2sbr" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
     </div>
@@ -94,11 +138,6 @@ get_header(); ?>
 
 </div><!-- #main -->
 </section><!-- #primary -->
-
-<img
-class="img-fluid"
-data-src="<?php echo get_template_directory_uri()?>/../wp-bootstrap-starter-child/assets/images/banner-illustration.png"
-alt="Single Temas">
 
 <?php
 
