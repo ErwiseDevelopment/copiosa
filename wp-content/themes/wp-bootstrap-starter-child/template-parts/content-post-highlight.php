8af070996b00e812e2a161b00d5c434b470bf975
Fabio de Melo <fabiomessias98@outlook.com>
2022-07-03 22:43:41 -0300

    Atualizado secao post destacado na pagina home

--- wp-content/themes/wp-bootstrap-starter-child/template-parts/content-post-highlight.php
+++ wp-content/themes/wp-bootstrap-starter-child/template-parts/content-post-highlight.php
@@ -12,54 +12,86 @@
                 </h2>
             </div>
 
-            <div class="col-12">
+            <?php
+                $args = array(
+                    'posts_per_page' => 1,
+                    'post_type'      => 'post',
+                    'category_name'  => 'destaque-home'
+                );
 
-                <div class="row">
+                $post_highlight = new WP_Query( $args );
 
-                    <div class="col-lg-8">
-                        <img
-                        class="img-fluid"
-                        src="<?php echo get_home_url( null, '/wp-content/uploads/2022/06/post-highlight-image.png' ) ?>"
-                        alt="Imagem destacada">
-                    </div>
-
-                    <div class="col-lg-4 mt-3 mt-lg-0">
-
-                        <div class="l-post-highlight__box u-box-shadow-pattern u-bg-folk-white p-3 p-lg-5">
-                            
-                            <h2 class="l-post-highlight__title u-font-weight-bold u-color-folk-squid-ink">
-                                Irmãs professam
-                                votos temporários e
-                                perpétuos em festa dos
-                                32 anos da Congregação
-                            </h2>
-
-                            <p class="u-font-size-12 xxl:u-font-size-15 u-font-weight-bold text-uppercase u-color-folk-golden">
-                                10 DE DEZEMBRO DE 2021
-                            </p>
-
-                            <p class="u-font-size-14 xxl:u-font-size-18 u-font-weight-regular u-color-folk-aluminium mt-4">
-                                Aconteceu no dia 07 de dezembro na nossa chácara, 
-                                no distrito de Uvaia, em Ponta Grossa, a celebração 
-                                dos Votos Temporários das Irmãs Amanda, Irmã Bruna, 
-                                Irmã Criciele, Irmã Gabriele, [...]
-                            </p>
+                if( $post_highlight->have_posts() ) :
+                    while( $post_highlight->have_posts() ) : $post_highlight->the_post();
+            ?>
+                        <div class="col-12">
 
                             <div class="row">
 
-                                <div class="col-7 mt-3 pt-3">
+                                <div class="col-lg-8">
+                                    <!-- <img
+                                    class="img-fluid"
+                                    src="<php echo get_home_url( null, '/wp-content/uploads/2022/06/post-highlight-image.png' ) ?>"
+                                    alt="Imagem destacada"> -->
 
-                                    <a
-                                    class="w-100 u-box-shadow-pattern d-block u-font-size-18 u-font-weight-bold u-font-family-nunito text-center text-decoration-none u-color-folk-white u-bg-folk-golden hover:u-bg-folk-squid-ink py-3"
-                                    href="#">
-                                        Ler mais
-                                    </a>
+                                    <?php
+                                        $alt_title = get_the_title();
+
+                                        the_post_thumbnail( 'post-thumbnail',
+                                            array(
+                                                'class' => 'img-fluid',
+                                                'alt'   => $alt_title
+                                        ));
+                                    ?>
+                                </div>
+
+                                <div class="col-lg-4 mt-3 mt-lg-0">
+
+                                    <div class="l-post-highlight__box u-box-shadow-pattern u-bg-folk-white p-3 p-lg-5">
+                                        
+                                        <h2 class="l-post-highlight__title u-font-weight-bold u-color-folk-squid-ink">
+                                            <!-- Irmãs professam
+                                            votos temporários e
+                                            perpétuos em festa dos
+                                            32 anos da Congregação -->
+
+                                            <?php the_title() ?>
+                                        </h2>
+
+                                        <p class="u-font-size-12 xxl:u-font-size-15 u-font-weight-bold text-uppercase u-color-folk-golden">
+                                            <!-- 10 DE DEZEMBRO DE 2021 -->
+                                            <?php echo get_date_format( get_the_date( 'd/m/Y', $post_highlight->ID ) ) ?>
+                                        </p>
+
+                                        <span class="d-block u-font-size-14 xxl:u-font-size-18 u-font-weight-regular u-color-folk-aluminium mt-4">
+                                            <!-- Aconteceu no dia 07 de dezembro na nossa chácara, 
+                                            no distrito de Uvaia, em Ponta Grossa, a celebração 
+                                            dos Votos Temporários das Irmãs Amanda, Irmã Bruna, 
+                                            Irmã Criciele, Irmã Gabriele, [...] -->
+                                            <?php echo(limit_words(the_content(), 25)); ?>
+                                        </span>
+
+                                        <div class="row">
+
+                                            <div class="col-7 mt-3 pt-3">
+
+                                                <a
+                                                class="w-100 u-box-shadow-pattern d-block u-font-size-18 u-font-weight-bold u-font-family-nunito text-center text-decoration-none u-color-folk-white u-bg-folk-golden hover:u-bg-folk-squid-ink py-3"
+                                                href="<?php the_permalink() ?>">
+                                                    Ler mais
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-            </div>
+            <?php
+                    endwhile;
+                endif;
+
+                wp_reset_query();
+            ?>
         </div>
     </div>
 </section>
