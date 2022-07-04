9c84fcde5c812d00c20543b21bf9d1840f2eae67
Fabio de Melo <fabiomessias98@outlook.com>
2022-07-04 11:42:37 -0300

    Atualizado os arquivos register-post-types e register-create-pages

--- wp-content/themes/wp-bootstrap-starter-child/inc/register-create-pages.php
+++ wp-content/themes/wp-bootstrap-starter-child/inc/register-create-pages.php
@@ -5,46 +5,13 @@ function single_create_page() {
     if( function_exists('acf_add_options_page') ) {	
         acf_add_options_page( 
             array( 
-                'page_title' => 'Página de exemplo', 
-                'menu_title' => 'Página de exemplo', 
-                'menu_slug'  => 'pagina_de_exemplo', 
+                'page_title' => 'Informações Gerais', 
+                'menu_title' => 'Informações Gerais', 
+                'menu_slug'  => 'Informações Gerais', 
                 'capability' => 'edit_posts', 
                 'position'   => '23,3', 
                 'redirect'   => false, 
                 'icon_url'   => 'dashicons-info' 
-        ));
-
-        acf_add_options_page( 
-            array( 
-                'page_title' => 'Informativo', 
-                'menu_title' => 'Informativo', 
-                'menu_slug'  => 'informativo', 
-                'capability' => 'edit_posts', 
-                'position'   => '23,4', 
-                'redirect'   => false, 
-                'icon_url'   => 'dashicons-info' 
-        ));
-
-        acf_add_options_page( 
-            array( 
-                'page_title' => 'Galeria', 
-                'menu_title' => 'Galeria', 
-                'menu_slug'  => 'galeria', 
-                'capability' => 'edit_posts', 
-                'position'   => '23,5', 
-                'redirect'   => false, 
-                'icon_url'   => 'dashicons-format-gallery' 
-        ));
-
-        acf_add_options_page( 
-            array( 
-                'page_title' => 'Campanha', 
-                'menu_title' => 'Campanha', 
-                'menu_slug'  => 'campanha', 
-                'capability' => 'edit_posts', 
-                'position'   => '23,6', 
-                'redirect'   => false, 
-                'icon_url'   => 'dashicons-format-gallery' 
         ));
     }
 }
