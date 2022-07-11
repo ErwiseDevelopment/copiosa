832109e7caa18211e0fe67153cba531d2c6cda41
ErwiseDevelopment <erwisedev@gmail.com>
2022-07-11 17:19:05 -0300

    adicionado post recuperacao noticias

--- wp-content/themes/wp-bootstrap-starter-child/template-parts/content-recovery-posts.php
+++ wp-content/themes/wp-bootstrap-starter-child/template-parts/content-recovery-posts.php
@@ -21,7 +21,7 @@
                         $args = array(
                             'posts_per_page' => 4,
                             'post_type'      => 'post',
-                            'category_name'  => 'vocacional',
+                            'category_name'  => 'recuperacao',
                             'order'          => 'DESC'
                         );
 
