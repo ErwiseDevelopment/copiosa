ea89c6623f715ada2a7f130f416b60529175f8c9
Fabio de Melo <fabiomessias98@outlook.com>
2022-07-13 09:22:40 -0300

    Finalizado dinamico da secao produto

--- wp-content/themes/wp-bootstrap-starter-child/template-parts/content-several.php
+++ wp-content/themes/wp-bootstrap-starter-child/template-parts/content-several.php
@@ -94,49 +94,57 @@
                                     <div class="row">
 
                                         <!-- loop -->
-                                        <?php for( $i = 0; $i < 3; $i++ ) { ?>
-                                            <div class="col-12 my-1">
+                                        <?php
+                                            if( have_rows( 'produtos', 'option' ) ) :
+                                                while( have_rows( 'produtos', 'option' ) ) : the_row();
+                                        ?>
+                                                    <div class="col-12 my-1">
 
-                                                <div class="row">
+                                                        <div class="row">
 
-                                                    <div class="col-4">
-                                                        <img
-                                                        class="img-fluid"
-                                                        src="<?php echo get_home_url( null, '/wp-content/uploads/2022/06/Prancheta-1-copiar-2-600x600-1.png' ) ?>"
-                                                        alt="">
-                                                    </div>
+                                                            <div class="col-4">
+                                                                <img
+                                                                class="img-fluid"
+                                                                src="<?php echo get_sub_field( 'imagem' ) ?>"
+                                                                alt="<?php echo get_sub_field( 'nome_descr' ) ?>">
+                                                            </div>
 
-                                                    <div class="col-8 pl-0">
-                                                        
-                                                        <h6 class="u-font-size-18 xxl:u-font-size-20 u-font-weight-semibold u-color-folk-bold-electric-blue mb-2">
-                                                            Planejamento
-                                                            Espiritual 2022
-                                                        </h6>
+                                                            <div class="col-8 pl-0">
+                                                                
+                                                                <h6 class="u-font-size-18 xxl:u-font-size-20 u-font-weight-semibold u-color-folk-bold-electric-blue mb-2">
+                                                                    <!-- Planejamento
+                                                                    Espiritual 2022 -->
+                                                                    <?php echo get_sub_field( 'nome_descr' ) ?>
+                                                                </h6>
 
-                                                        <p class="u-font-size-14 xxl:u-font-size-18 u-font-weight-semibold u-color-folk-golden">
-                                                            R$ 26,90
-                                                        </p>
-                                                    </div>
+                                                                <p class="u-font-size-14 xxl:u-font-size-18 u-font-weight-semibold u-color-folk-golden">
+                                                                    <!-- R$ 26,90 -->
+                                                                    R$ <?php echo get_sub_field( 'valor' ) ?>
+                                                                </p>
+                                                            </div>
 
-                                                    <div class="col-12 mt-1">
+                                                            <div class="col-12 mt-1">
 
-                                                        <div class="l-several__line"></div>
+                                                                <div class="l-several__line"></div>
 
-                                                        <div class="row justify-content-center">
+                                                                <div class="row justify-content-center">
 
-                                                            <div class="col-7 offset-2">
+                                                                    <div class="col-7 offset-2">
 
-                                                                <a
-                                                                class="w-100 u-box-shadow-pattern d-block u-font-size-12 xxl:u-font-size-16 u-font-weight-bold u-font-family-nunito text-center text-decoration-none u-color-folk-bold-electric-blue hover:u-color-folk-golden u-bg-folk-golden hover:u-bg-folk-squid-ink py-2"
-                                                                href="#">
-                                                                    Comprar
-                                                                </a>
+                                                                        <a
+                                                                        class="w-100 u-box-shadow-pattern d-block u-font-size-12 xxl:u-font-size-16 u-font-weight-bold u-font-family-nunito text-center text-decoration-none u-color-folk-bold-electric-blue hover:u-color-folk-golden u-bg-folk-golden hover:u-bg-folk-squid-ink py-2"
+                                                                        href="<?php echo get_sub_field( 'link_do_produto' ) ?>">
+                                                                            Comprar
+                                                                        </a>
+                                                                    </div>
+                                                                </div>
                                                             </div>
                                                         </div>
                                                     </div>
-                                                </div>
-                                            </div>
-                                        <?php } ?>
+                                        <?php
+                                                endwhile;
+                                            endif;
+                                        ?>
                                         <!--- end loop -->
                                     </div>
                                 </div>
