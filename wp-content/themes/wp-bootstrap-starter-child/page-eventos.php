1482c410f909465c9ba004179e39e6c6b34b7798
Fabio de Melo <fabiomessias98@outlook.com>
2022-07-13 08:17:21 -0300

    Ajustado o custom da pagina eventos

--- wp-content/themes/wp-bootstrap-starter-child/page-eventos.php
+++ wp-content/themes/wp-bootstrap-starter-child/page-eventos.php
@@ -53,7 +53,7 @@ get_header(); ?>
                     <?php 
                         $args = array(
                             'posts_per_page' => -1,
-                            'post_type'      => 'eventos',
+                            'post_type'      => 'evento',
                             'order'          => 'DESC'
                         );
 
@@ -62,71 +62,71 @@ get_header(); ?>
                         if( $events->have_posts() ) :
                             while( $events->have_posts() ) : $events->the_post();
                     ?>
-                        <div class="col-lg-6 my-3">
-
-                            <div class="row">
-
-                                <div class="col-md-4">
-
-                                    <!-- <img
-                                    class="img-fluid w-100 u-object-fit-cover"
-                                    src="<php echo get_home_url( null, '/wp-content/uploads/2022/06/Image.png' )?>"
-                                    alt=""> -->
-
-                                    <?php 
-                                        $alt_title = get_the_title();
-
-                                        the_post_thumbnail( 'post-thumbnail',
-                                            array(
-                                                'class' => 'img-fluid w-100 u-object-fit-cover',
-                                                'alt'   => $alt_title
-                                        ));
-                                    ?>
-
-                                    <p class="u-font-size-12 u-font-weight-bold u-color-folk-golden mt-1 mb-0">
-                                        <!-- 16 de março, 2022 -->
-                                        <?php echo get_field( 'data' ) ?>
-                                    </p>
-                                </div>
-
-                                <div class="col-md-8">
+                                <div class="col-lg-6 my-3">
 
                                     <div class="row">
 
-                                        <div class="col-md-2">
-                                            <p class="u-font-size-12 u-font-weight-bold u-color-folk-golden mb-1">
-                                                <!-- 19:00 -->
-                                                <?php echo get_field( 'horario' ) ?>
+                                        <div class="col-md-4">
+
+                                            <!-- <img
+                                            class="img-fluid w-100 u-object-fit-cover"
+                                            src="<php echo get_home_url( null, '/wp-content/uploads/2022/06/Image.png' )?>"
+                                            alt=""> -->
+
+                                            <?php 
+                                                $alt_title = get_the_title();
+
+                                                the_post_thumbnail( 'post-thumbnail',
+                                                    array(
+                                                        'class' => 'img-fluid w-100 u-object-fit-cover',
+                                                        'alt'   => $alt_title
+                                                ));
+                                            ?>
+
+                                            <p class="u-font-size-12 u-font-weight-bold u-color-folk-golden mt-1 mb-0">
+                                                <!-- 16 de março, 2022 -->
+                                                <?php echo get_field( 'data' ) ?>
                                             </p>
                                         </div>
 
-                                        <div class="col-md-10">
-                                            <p class="u-font-size-14 u-font-weight-regular u-color-folk-aluminium mb-1">
-                                                <!-- Lorem ipsum dolor sit amet consectetur. -->
-                                                <?php echo get_field( 'endereco' ) ?>
-                                            </p>
-                                        </div>
-                                    </div>
+                                        <div class="col-md-8">
 
-                                    <h3 class="u-font-weight-bold u-color-folk-bold-electric-blue">
-                                        <!-- Lorem ipsum dolor sit -->
-                                        <?php the_title() ?>
-                                    </h3>
-                               
-                                    <div class="row">
+                                            <div class="row">
 
-                                        <div class="col-5">
+                                                <div class="col-md-2">
+                                                    <p class="u-font-size-12 u-font-weight-bold u-color-folk-golden mb-1">
+                                                        <!-- 19:00 -->
+                                                        <?php echo get_field( 'horario' ) ?>
+                                                    </p>
+                                                </div>
 
-                                            <a 
-                                            class="w-100 rounded d-block u-font-size-12 u-font-weight-bold text-center text-decoration-none u-color-folk-white u-bg-folk-golden hover:u-bg-folk-medium-electric-blue py-2"
-                                            href="<?php the_permalink() ?>">
-                                                Ver evento
-                                            </a>
+                                                <div class="col-md-10">
+                                                    <p class="u-font-size-14 u-font-weight-regular u-color-folk-aluminium mb-1">
+                                                        <!-- Lorem ipsum dolor sit amet consectetur. -->
+                                                        <?php echo get_field( 'endereco' ) ?>
+                                                    </p>
+                                                </div>
+                                            </div>
+
+                                            <h3 class="u-font-weight-bold u-color-folk-bold-electric-blue">
+                                                <!-- Lorem ipsum dolor sit -->
+                                                <?php the_title() ?>
+                                            </h3>
+                                    
+                                            <div class="row">
+
+                                                <div class="col-5">
+
+                                                    <a 
+                                                    class="w-100 rounded d-block u-font-size-12 u-font-weight-bold text-center text-decoration-none u-color-folk-white u-bg-folk-golden hover:u-bg-folk-medium-electric-blue py-2"
+                                                    href="<?php the_permalink() ?>">
+                                                        Ver evento
+                                                    </a>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
                     <?php
                             endwhile;
                         endif;
@@ -144,11 +144,6 @@ get_header(); ?>
 
 </div><!-- #main -->
 </section><!-- #primary -->
-
-<img
-class="img-fluid"
-data-src="<?php echo get_template_directory_uri()?>/../wp-bootstrap-starter-child/assets/images/banner-illustration.png"
-alt="Single Temas">
 
 <?php
 
