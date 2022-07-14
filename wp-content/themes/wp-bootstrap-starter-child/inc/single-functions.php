15ce3c8082bf8047192fd8852fb0340fc1a94509
Fabio de Melo <fabiomessias98@outlook.com>
2022-07-14 01:11:01 -0300

    Implementado filtro por mes nos eventos

--- wp-content/themes/wp-bootstrap-starter-child/inc/single-functions.php
+++ wp-content/themes/wp-bootstrap-starter-child/inc/single-functions.php
@@ -27,6 +27,31 @@ function get_date_format( $date ) {
     return $date_format;
 }
 
+function get_date_month( $month_current ) {
+
+    $months = array(
+        '01' => 'Janeiro',
+        '02' => 'Fevereiro',
+        '03' => 'Março',
+        '04' => 'Abril',
+        '05' => 'Maio',
+        '06' => 'Junho',
+        '07' => 'Julho',
+        '08' => 'Agosto',
+        '09' => 'Setembro',
+        '10' => 'Outubro',
+        '11' => 'Novembro',
+        '12' => 'Dezembro',
+    );
+
+    foreach( $months as $key => $value ) {
+        if( $key == $month_current ) 
+            $month_current = $value;
+    }
+
+    return $month_current;
+}
+
 function get_category_editorials( $categories ) {
     $editorials = array(
         'Institucional',
