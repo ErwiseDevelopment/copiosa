17bc827d2173846b36b14db97ead73e7f27a6995
ErwiseDevelopment <erwisedev@gmail.com>
2022-07-19 10:49:06 -0300

    atualização 19/07

--- wp-content/themes/wp-bootstrap-starter-child/template-parts/content-posts.php
+++ wp-content/themes/wp-bootstrap-starter-child/template-parts/content-posts.php
@@ -115,7 +115,7 @@
                                                     <!-- O Centro Âncora promoveu no dia 14 de
                                                         outubro, um workshop, intitulado “Teu 
                                                         Olhar” voltado para os religiosos (as) [...] -->
-                                                    <?php
+                                                        <?php
                                                         the_excerpt();
                                                     ?>
                                                 </span>
