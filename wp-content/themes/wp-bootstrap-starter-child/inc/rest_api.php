4d4ea205f10d866f0f1915e0428858008e030060
Fabio de Melo <fabiomessias98@outlook.com>
2022-07-21 20:43:35 -0300

    Teste de imagem do post API

--- wp-content/themes/wp-bootstrap-starter-child/inc/rest_api.php
+++ wp-content/themes/wp-bootstrap-starter-child/inc/rest_api.php
@@ -49,14 +49,33 @@ function add_thumbnail_to_JSON() {
 //     return $feat_img_array[0];
 // }
 
-function post_featured_image_json( $data, $post, $context ) {
-    $featured_image_id = $data->data['featured_media']; // get featured image id
-    $featured_image_url = wp_get_attachment_image_src( $featured_image_id, 'original' ); // get url of the original size
+// function post_featured_image_json( $data, $post, $context ) {
+//     $featured_image_id = $data->data['featured_media']; // get featured image id
+//     $featured_image_url = wp_get_attachment_image_src( $featured_image_id, 'original' ); // get url of the original size
 
-    if( $featured_image_url ) {
-      $data->data['featured_image_src'] = $featured_image_url[0];
-    }
+//     if( $featured_image_url ) {
+//       $data->data['featured_image_src'] = $featured_image_url[0];
+//     }
   
-    return $data;
-  }
-add_filter( 'rest_prepare_post', 'post_featured_image_json', 10, 3 );
+//     return $data;
+//   }
+// add_filter( 'rest_prepare_post', 'post_featured_image_json', 10, 3 );
+
+add_action('rest_api_init', 'register_rest_images' );
+function register_rest_images(){
+    register_rest_field( array('post'),
+        'fimg_url',
+        array(
+            'get_callback'    => 'get_rest_featured_image',
+            'update_callback' => null,
+            'schema'          => null,
+        )
+    );
+}
+function get_rest_featured_image( $object, $field_name, $request ) {
+    if( $object['featured_media'] ){
+        $img = wp_get_attachment_image_src( $object['featured_media'], 'app-thumb' );
+        return $img[0];
+    }
+    return false;
+}
