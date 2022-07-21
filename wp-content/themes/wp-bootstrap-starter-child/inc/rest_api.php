2ffe4adc7dccb7c9c3f8f695f7611c2f2e8ea1f0
Fabio de Melo <fabiomessias98@outlook.com>
2022-07-21 19:19:44 -0300

    Teste de imagem do post API

--- wp-content/themes/wp-bootstrap-starter-child/inc/rest_api.php
+++ wp-content/themes/wp-bootstrap-starter-child/inc/rest_api.php
@@ -1,7 +1,7 @@
 <?php
 
-
 //nao deu certo
+add_action( 'rest_api_init', 'add_thumbnail_to_JSON' );
 function add_thumbnail_to_JSON() {
     register_rest_field( 
         'post',
@@ -14,10 +14,21 @@ function add_thumbnail_to_JSON() {
     );
 }
 
-add_action( 'rest_api_init', 'add_thumbnail_to_JSON' );
-
 //testando
-function get_featured_image_api_field($post_id)
-{
-    return get_the_post_thumbnail_url(null, 'post-thumb-small');
+// function get_featured_image_api_field($post_id)
+// {
+//     return get_the_post_thumbnail_url(null, 'post-thumb-small');
+// }
+
+function post_featured_image_json( $data, $post, $context ) {
+    $featured_image_id = $data->data['featured_media']; // get featured image id
+    $featured_image_url = wp_get_attachment_image_src( $featured_image_id, 'original' ); // get url of the original size
+
+    if( $featured_image_url ) {
+        $data->data['featured_image_src'] = $featured_image_url[0];
+    }
+
+    return $data;
 }
+
+add_filter( 'rest_prepare_post', 'post_featured_image_json', 10, 3 );
