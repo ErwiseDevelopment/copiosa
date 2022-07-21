0aaa5ee2beecfcf524e5b6ac82a1dd7f9412e560
ErwiseDevelopment <erwisedev@gmail.com>
2022-07-21 12:12:37 -0300

    adicionado pagina de opcoes campanha

--- wp-content/themes/wp-bootstrap-starter-child/inc/register-create-pages.php
+++ wp-content/themes/wp-bootstrap-starter-child/inc/register-create-pages.php
@@ -14,5 +14,18 @@ function single_create_page() {
                 'icon_url'   => 'dashicons-info' 
         ));
     }
+
+    if( function_exists('acf_add_options_page') ) {	
+        acf_add_options_page( 
+            array( 
+                'page_title' => 'Campanha', 
+                'menu_title' => 'Campanha', 
+                'menu_slug'  => 'Campanha', 
+                'capability' => 'edit_posts', 
+                //'position'   => '20,3', 
+                'redirect'   => false, 
+                'icon_url'   => 'dashicons-excerpt-view' 
+        ));
+    }
 }
 add_action( 'init', 'single_create_page' );
