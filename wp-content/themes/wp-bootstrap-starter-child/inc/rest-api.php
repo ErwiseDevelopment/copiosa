471716f9a1152750f9e388aa2256eb2ef6a8aa70
Fabio de Melo <fabiomessias98@outlook.com>
2022-07-21 21:32:10 -0300

    Adicionado a data de postagem do rest_api

--- wp-content/themes/wp-bootstrap-starter-child/inc/rest-api.php
+++ wp-content/themes/wp-bootstrap-starter-child/inc/rest-api.php
@@ -1,8 +1,6 @@
 <?php
 
-//nao deu certo
-add_action( 'rest_api_init', 'add_thumbnail_to_JSON' );
-function add_thumbnail_to_JSON() {
+function add_thumbnail_rest_api() {
     register_rest_field( 
         'post',
         'featured_image_src',
@@ -14,77 +12,21 @@ function add_thumbnail_to_JSON() {
     );
 }
 
+add_action( 'rest_api_init', 'add_thumbnail_rest_api' );
+
 function get_image_src( $object, $field_name, $request ) {
     $feat_img_array = wp_get_attachment_image_src(
-        $object['featured_media'], // Image attachment ID
-        'full',  // Size.  Ex. "thumbnail", "large", "full", etc..
-        true // Whether the image should be treated as an icon.
+        $object['featured_media'],
+        'full', 
+        true
     );
     return $feat_img_array[0];
 }
 
-// function post_featured_image_json( $data, $post, $context ) {
-//     $featured_image_id = $data->data['featured_media']; // get featured image id
-//     $featured_image_url = wp_get_attachment_image_src( $featured_image_id, 'original' ); // get url of the original size
+function add_post_date_rest_api( $data, $post ) {
+    $data->data['post_date'] = get_the_date( 'd/m/Y', $post->ID );
 
-//     if( $featured_image_url ) {
-//         $data->data['featured_image_src'] = $featured_image_url[0];
-//     }
+    return $data;
+}
 
-//     return $data;
-// }
-
-// add_filter( 'rest_prepare_post', 'post_featured_image_json', 10, 3 );
-
-// function get_featured_image_api_field($post_id)
-// {
-//     return get_the_post_thumbnail_url(null, 'post-thumb-small');
-// }
-
-// function deepIncludeFields( $item, $key, $postTypes ) {
-//     if ( isset( $item->post_type ) && in_array( $item->post_type, $postTypes ) ) {
-//         $item->acf = get_fields( $item->ID );
-//         $item->image = get_the_post_thumbnail_url($item->ID);
-//         $item->categories = wp_get_post_categories($item->ID, array( 'fields' => 'names' ));
-//     }
-// }
-
-// function get_image_src( $object, $field_name, $request ) {
-//     $feat_img_array = wp_get_attachment_image_src(
-//         $object['featured_media'], // Image attachment ID
-//     'full',  // Size.  Ex. "thumbnail", "large", "full", etc..
-//         true // Whether the image should be treated as an icon.
-//     );
-//     return $feat_img_array[0];
-// }
-
-// function post_featured_image_json( $data, $post, $context ) {
-//     $featured_image_id = $data->data['featured_media']; // get featured image id
-//     $featured_image_url = wp_get_attachment_image_src( $featured_image_id, 'original' ); // get url of the original size
-
-//     if( $featured_image_url ) {
-//       $data->data['featured_image_src'] = $featured_image_url[0];
-//     }
-  
-//     return $data;
-//   }
-// add_filter( 'rest_prepare_post', 'post_featured_image_json', 10, 3 );
-
-// add_action('rest_api_init', 'register_rest_images' );
-// function register_rest_images(){
-//     register_rest_field( array('post'),
-//         'fimg_url',
-//         array(
-//             'get_callback'    => 'get_rest_featured_image',
-//             'update_callback' => null,
-//             'schema'          => null,
-//         )
-//     );
-// }
-// function get_rest_featured_image( $object, $field_name, $request ) {
-//     if( $object['featured_media'] ){
-//         $img = wp_get_attachment_image_src( $object['featured_media'], 'app-thumb' );
-//         return $img[0];
-//     }
-//     return false;
-// }
+add_filter('rest_prepare_post', 'add_post_date_rest_api', 10, 3);
