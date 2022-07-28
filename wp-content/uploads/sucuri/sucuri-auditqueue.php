eb45ec06572215842fba9960572fff3af71133a0
ErwiseDevelopment <erwisedev@gmail.com>
2022-07-28 15:05:01 -0300

    seguranca

--- wp-content/uploads/sucuri/sucuri-auditqueue.php
+++ wp-content/uploads/sucuri/sucuri-auditqueue.php
@@ -5,3 +5,4 @@
 exit(0);
 ?>
 1659030179_018:"Warning: erwise, 127.0.0.1; Plugin activated: Sucuri Security - Auditing, Malware Scanner and Hardening (v1.8.32; sucuri-scanner\/sucuri.php)"
+1659031336_5004:"Warning: erwise, 127.0.0.1; Plugin deactivated: SecuPress Free \u2014 WordPress Security (v2.2; secupress\/secupress.php)"
